f2b83e028458d3bb236d7af1f933481d30ac9b08
SaqqaraWright <wrightsaqqara@gmail.com>
2022-05-04 15:02:48 -0400

    initial commit

---
+++ prop-it-up/src/components/PersonCard.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class PersonCard extends Component {
+    render() {
+        return <div>
+            <div>
+                <h1>Mae, Sally</h1>
+                <p>Age:100</p>
+                <p>Hair Color: Green</p>
+            </div>
+            <div>
+                <h1>Holly, Jane</h1>
+                <p>Age:40</p>
+                <p>Hair Color: Green</p>
+            </div>
+            <div>
+                <h1>Ray, Millard</h1>
+                <p>Age:55</p>
+                <p>Hair Color: Green</p>
+            </div>
+            <div>
+                <h1>Smith, Marla</h1>
+                <p>Age:25</p>
+                <p>Hair Color: Green</p>
+            </div>
+        </div>;
+    }
+}
+    
+export default PersonCard;
